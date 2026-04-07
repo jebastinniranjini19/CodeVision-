87501fc98309813b51666dc1ffffe639b51bdf38
jebastinniranjini19 <jebastinniranjini@gmail.com>
2026-04-07 11:01:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("loginForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  // Simple demo login redirect
+  window.location.href = "dashboard.html";
+});
